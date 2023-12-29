3da4d6660dd0ab2f445c64c81ad50e9f23589cdf
sunilcygnet <153914228+sunilcygnet@users.noreply.github.com>
2023-12-29 20:17:41 +0530

    Update SupplierWorkPermitModel.cs

--- Quest_WebAPI/Models/SupplierWorkPermitModel.cs
+++ Quest_WebAPI/Models/SupplierWorkPermitModel.cs
@@ -1,12 +1,11 @@
-﻿namespace WebAPI.Models;
+namespace WebAPI.Models;
 
 public class SupplierWorkPermitModel
 {
     public string Title { get; set; }
-    public List<Item> WorkPermitData { get; set; }
-
-   
+    public List<Item> WorkPermitData { get; set; }   
     public List<RadioSection> Radios { get; set; }
     public CheckboxSection Checkbox { get; set; }
     public List<TextboxSection> Textboxes { get; set; }
+    public List<SignatureSection> SignatureBoxes { get; set; }
 }
